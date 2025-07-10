3b6bbeeff66999b2ce4b9a75182a8504da0e6833
Vxrpenter <Vxrpenter@users.noreply.github.com>
2025-07-10 22:43:30 +0200

    Updated GroupPriority with some numbers for later comparisons

--- src/main/kotlin/io/github/vxrpenter/enum/GroupPriority.kt
+++ src/main/kotlin/io/github/vxrpenter/enum/GroupPriority.kt
@@ -18,11 +18,31 @@
 
 package io.github.vxrpenter.enum
 
-enum class GroupPriority {
-    NONE,
-    MINIMAL,
-    LOW,
-    MIDDLE,
-    HIGH,
-    HIGHEST,
+enum class GroupPriority(val priority: Int) {
+    NONE(0),
+    MINIMAL(1),
+    LOW(2),
+    MIDDLE(3),
+    HIGH(4),
+    HIGHEST(5);
+
+    companion object {
+        /**
+         * Finds the specified enum name from its GroupPriority.
+         *
+         * @param value The GroupPriority e.g. (4, 5, etc.)
+         * @see io.github.vxrpenter.enum.GroupPriority
+         * @return the GroupPriority (nullable)
+         */
+        fun findEnum(value: Int): GroupPriority? = GroupPriority.entries.find { it.priority == value }
+
+        /**
+         * Finds the specified GroupPriority from its enum
+         *
+         * @param enum The enum e.g. (HIGH, HIGHEST etc.)
+         * @see io.github.vxrpenter.enum.GroupPriority
+         * @return the GroupPriority (nullable)
+         */
+        fun findValue(enum: GroupPriority): Int? = GroupPriority.entries.find { it.name == enum.name }?.priority
+    }
 }
