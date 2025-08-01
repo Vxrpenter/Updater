91b206bf256e15f8ca9d1058895891593e7a9403
Vxrpenter <Vxrpenter@users.noreply.github.com>
2025-08-01 19:30:23 +0200

    Fix copyright issue

--- src/main/kotlin/io/github/vxrpenter/updater/upstream/ModrinthProjectType.kt
+++ src/main/kotlin/io/github/vxrpenter/updater/upstream/ModrinthProjectType.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Vxrpenter and the Updater contributors
+ * Copyright (c) 2025 Vxrpenter and the Updater contributors
  *
  * Licenced under the MIT License, any non-license compliant usage of this file(s) content
  * is prohibited. If you did not receive a copy of the license with this file, you
@@ -18,32 +18,11 @@
 
 package io.github.vxrpenter.updater.upstream
 
-enum class ModrinthProjectType(val type: String) {
+enum class ModrinthProjectType(val value: String) {
     MOD("mod"),
     RESOURCEPACK("resourcepack"),
     DATAPACK("datapack"),
     SHADER("shader"),
     MODPACK("modpack"),
-    PLUGIN("plugin");
-
-    @Suppress("unused")
-    companion object {
-        /**
-         * Finds the specified enum name from its ModrinthProjectType.
-         *
-         * @param value The ModrinthProjectType e.g. (modpack, plugin, etc.)
-         * @see io.github.vxrpenter.updater.schema.ClassifierPriority
-         * @return the ModrinthProjectType (nullable)
-         */
-        fun findEnum(value: String): ModrinthProjectType? = entries.find { it.type == value }
-
-        /**
-         * Finds the specified ModrinthProjectType from its enum
-         *
-         * @param enum The enum e.g. (MODPACK, PLUGIN etc.)
-         * @see io.github.vxrpenter.updater.enum.ModrinthProjectType
-         * @return the ModrinthProjectType (nullable)
-         */
-        fun findValue(enum: ModrinthProjectType): String? = entries.find { it.name == enum.name }?.type
-    }
+    PLUGIN("plugin")
 }
